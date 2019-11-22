6df46850e6b1cb3720901875f5b5bcb38e3dd8c3
Sergiu Verciuc <sergiu.verciuc@cegeka.com>
2019-11-22 15:20:53 +0200

    Project setup

---
+++ Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cegeka.Guild.Pokeverse.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cegeka.Guild.Pokeverse.DAL
+{
+    public class Class1
+    {
+    }
+}
